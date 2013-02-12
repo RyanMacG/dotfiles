fafd3c02a5ce11a3e5963cef0712953024942cae
Ryan MacGillivray <rmacg89@gmail.com>
2013-02-12 15:20:56 +0000

    removed slate as it broke slate.app

--- symlinksyo.sh
+++ symlinksyo.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 ############################
 # .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# This script creates symlinks from the home directory to any desired dotfiles in ~/.dotfiles
 ############################
 
 ########## Variables
 
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh gitconfig gitignore_global hgrc gemrc hgignore_global railsrc slate"    # list of files/folders to symlink in homedir
+files="bashrc vimrc vim zshrc oh-my-zsh gitconfig gitignore_global hgrc gemrc hgignore_global railsrc"    # list of files/folders to symlink in homedir
 
 ##########
 
